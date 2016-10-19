bc4abd1b3266ee41830d46352c5d093e3c2cf182
Bima Tri P <satya24@student.uns.ac.id>
2016-10-19 13:03:11 +0700

    ganti title

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -10,7 +10,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
+    <p>SILAKAN LOGIN</p>
 
     <div class="row">
         <div class="col-lg-5">
